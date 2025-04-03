5c9135089b474b4b102ee5dbfd5cebe7f4de7a0e
ROHITH-M10 <rohithmemail@gmail.com>
2025-04-03 10:06:29 +0530

    remove state comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Navbar({ 
-  onHomeClick, 
-  // setAdminUrl, 
-  // handleLogin, 
-  // selectedServerOption, 
-  // setSelectedServerOption, 
-  // handleServerSelect, 
-  // setIsServerDropdownOpen, 
-  // isServerDropdownOpen, 
-  // toggleServerDropdown 
+  onHomeClick
 }) {
 
 
@@ -18,36 +10,7 @@ function Navbar({
       <button onClick={onHomeClick} className="home-button">
         <i className="fa fa-home"></i>
       </button>
-      {/* <div className="menu-container">
-        <i onClick={toggleServerDropdown} className="fa fa-server"></i>
-        <i onClick={handleLogin} className="fa fa-user-circle"></i>
 
-        {isServerDropdownOpen && (
-          <div className="menu-dropdown">
-            <div 
-              className={`menu-dropdown-item ${selectedServerOption === "Web Forms" ? "server-selected" : ""}`} 
-              onClick={() => handleServerSelect("Web Forms")}
-            >
-              Web Forms
-            </div>
-            <div 
-              className={`menu-dropdown-item ${selectedServerOption === "Registries" ? "server-selected" : ""}`} 
-              onClick={() => handleServerSelect("Registries")}
-            >
-              Registries
-            </div>
-
-            <div 
-              className={`menu-dropdown-item ${selectedServerOption === "Staging / Test" ? "server-selected" : ""}`}
-              onClick={() => handleServerSelect("Staging / Test")}
-            >
-              Staging / Test
-            </div>
-
-
-          </div>
-        )}
-      </div> */}
     </div>
   );
 }
