9a20e108344812743aac922897655ffd64a99b85
ROHITH-M10 <rohithmemail@gmail.com>
2025-04-08 11:47:50 +0530

    drop down change style

--- src/components/Guest.jsx
+++ src/components/Guest.jsx
@@ -1,31 +1,36 @@
 import React from "react";
-import Dropdown from "./Dropdown";
-// Import combined forms data
+import { Collapse } from "antd";
 import Forms from "../data/Forms.json";
 
-function Guest({ 
-  openDropdown, 
-  toggleDropdown, 
-  handleSelect, 
-  selectedUrl 
-}) {
+function Guest({ handleSelect, selectedUrl }) {
+  const items = Object.entries(Forms).map(([category, links], index) => ({
+    key: String(index + 1),
+    label: category,
+    children: (
+      <ul className="link-list">
+        {Object.entries(links).map(([name, url]) => (
+          <li key={name}>
+            <div
+              className="link-icon"
+              onClick={() => handleSelect(url)}
+            >
+              {name}
+            </div>
+          </li>
+        ))}
+      </ul>
+    ),
+  }));
+
   return (
     <div className="guest-page">
-      {/* Dropdowns */}
-      <div className="guest-dropdown-button-container">
-        {Object.entries(Forms).map(([category, items]) => (
-          <Dropdown
-            key={category}
-            title={category}
-            items={items}
-            isOpen={openDropdown === category}
-            toggleDropdown={toggleDropdown}
-            onSelect={handleSelect}
-          />
-        ))}
+      {console.log(items)}
+      {/* Collapse Panels */}
+      <div className="guest-collapse-container">
+        <Collapse items={items} />
       </div>
 
-      {/* iFrame to display selected page */}
+      {/* iFrame for selected form */}
       {selectedUrl && (
         <div className="guest-iframe-container">
           <iframe src={selectedUrl} title="content-frame"></iframe>
